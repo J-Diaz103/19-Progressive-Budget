82f4286f1a1b034d40a56efb79639c60298133b0
J Diaz <j.e.diaz@icloud.com>
2021-08-22 21:50:12 -0700

    bug

--- server.js
+++ server.js
@@ -28,5 +28,3 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-// mongodb+srv://Jesus-admin:1likePandas^w^@cluster0.ghkzi.mongodb.net/budgetdb?retryWrites=true&w=majority
